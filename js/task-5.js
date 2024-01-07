92a33a220a62116503bb75e1a0043101a7ddb15d
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-01-07 19:36:46 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,37 @@
+
+// змінює колір фону елемента < body > через інлайн - стиль по кліку на button.change - color 
+// і задає це значення кольору текстовим вмістом для span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+
+
+const btnColor = document.querySelector('.change-color');
+const bodyElem = document.querySelector('body');
+const spanElem = document.querySelector('.color')
+
+btnColor.addEventListener('click', onButtonClick);
+
+
+function onButtonClick() {
+  const bgColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = bgColor;
+  spanElem.textContent = `${bgColor}`
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
-}
+     .padStart(6, 0)}`;
+ 
+     }
+
+
+
+  
+
+
+
