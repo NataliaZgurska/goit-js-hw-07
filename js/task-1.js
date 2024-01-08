0919f1673cfbe2135291166a23180cdd57662923
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-01-08 09:35:57 +0200

    all tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,3 @@
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
-// елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
-
 const ulElem = document.querySelector('#categories');
 const liElems = ulElem.querySelectorAll('.item')
 console.log('Number of categories:', liElems.length);
--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,34 @@
-// Є input, у який користувач вводить бажану кількість елементів. 
-
-// Після натискання на кнопку Create має рендеритися(додаватися в DOM) колекція з відповідною кількістю елементів 
-// і очищатися значення в інпуті.
-
-// При повторному натисканні на кнопку Create поверх старої колекції має рендеритись нова.
-
-// Після натискання на кнопку Destroy колекція елементів має очищатися.
-
-// Після натискання користувачем на кнопку Create треба провалідувати значення в input, воно має бути в межах від 1 до 100 включно. 
-// Тільки якщо воно задоволяє умову, мають додаватися нові < div > елементи в DOM.
-
-// Для рендеру елементів на сторінці створи функцію createBoxes(amount), 
-// яка приймає один параметр — число, що зберігає кількість елементів для рендеру.
-// Функція має створювати стільки < div > елементів, скільки вказано в параметрі amount і додавати їх у DOM 
-// дочірніми елементами для div#boxes.
-
-// Розміри першого <div> елемента мають бути 30px на 30px.
-// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
-// Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
-
 const box = document.querySelector('#boxes');
-
 const inputElem = document.querySelector('#controls')
-inputElem.firstElementChild.addEventListener('input', onInputClick);
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+
+buttonCreate.addEventListener('click', onButtonCreateClick)
+buttonDestroy.addEventListener('click', onButtonDestroyClick)
 
 let inputValue;
+let amount;
 let boxArray;
 
-function onInputClick() {
+function onButtonCreateClick() {
   inputValue = Number(inputElem.firstElementChild.value);
-   createBoxes(inputValue);
-  box.innerHTML = boxArray.join(' ');
+
+if (inputValue >= 1 && inputValue <= 100) {
+  amount = inputValue;
+} else {
+  if (inputValue > 100) {
+    amount = 0;
+  } 
+}
+  createBoxes(amount);
+  box.innerHTML = boxArray.join(' '); 
+  inputElem.firstElementChild.value = '';
 }
 
+
+function onButtonDestroyClick() {
+  box.innerHTML = '';
+}
 
 function createBoxes(amount) {
   boxArray = [];
@@ -48,12 +44,6 @@ function createBoxes(amount) {
  }
   return boxArray;
 }
-
-// createBoxes(5);
-// box.innerHTML = boxArray.join(' ');
-
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
